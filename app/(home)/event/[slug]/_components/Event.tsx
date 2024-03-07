8c66e30188be1900baef83c4d78b0367860593a2
Edo <1122140027@global.ac.id>
2024-03-08 01:32:04 +0700

    add embed thing in event/:slug/

--- app/(home)/event/[slug]/_components/Event.tsx
+++ app/(home)/event/[slug]/_components/Event.tsx
@@ -1,12 +1,21 @@
+import { BASE_URL_EVENT } from "@/constants/event";
 import { Event } from "@prisma/client";
 import React from "react";
 
 const Event = ({ slug, title }: Partial<Event>) => {
   return (
-    <div>
-      <p>{title}</p>
-      <p>{slug}</p>
-    </div>
+    <section className="section h-[712px] ">
+      <article className="container h-full w-full">
+        <iframe
+          className="w-full h-full"
+          title={title}
+          sandbox="allow-scripts allow-same-origin"
+          aria-hidden="false"
+          src={BASE_URL_EVENT + slug}
+          style={{ border: "none" }}
+        />
+      </article>
+    </section>
   );
 };
 
