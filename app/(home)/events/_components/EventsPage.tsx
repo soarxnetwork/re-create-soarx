d35dc148acdd2abd1a9935dacdc6ffc49cb549b2
Akshat <akshatjain481@gmail.com>
2024-04-01 15:24:40 +0530

    fixed deployment error

--- app/(home)/events/_components/EventsPage.tsx
+++ app/(home)/events/_components/EventsPage.tsx
@@ -1,19 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { AiOutlineCalendar } from "react-icons/ai";
-import Image from "next/image";
-import { formatDate } from "@/utils/formatDate";
-import { Event } from "@prisma/client";
-import Link from "next/link";
+import React from 'react';
 import Events from "./Events";
-import Pagination from "@/app/(admin)/_components/Pagination";
-import { useSearchParams } from "next/navigation";
-import HeadingSecondDsa from "../../campus-ambassador/_components/HeadingSecondDsa";
-import { CiBatteryEmpty } from "react-icons/ci";
+
 import { getOngoingEvents, getPastEvents } from "@/services/events";
 import TemporaryEmptyEvents from "@/components/TemporaryEmptyEvents";
 
 
-const EventPage = async () => {
+const EventPage = async (events: any) => {
 
   const pastEvents = await getPastEvents()
   const ongoingEvents = await getOngoingEvents()
