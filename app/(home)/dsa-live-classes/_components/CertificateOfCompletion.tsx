a5f86dd7dcb801b4c9c2365cb5518fabcb49ad2b
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-04-20 14:15:15 +0530

    certfictes of completion

--- app/(home)/dsa-live-classes/_components/CertificateOfCompletion.tsx
+++ app/(home)/dsa-live-classes/_components/CertificateOfCompletion.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const CertificateOfCompletion = () => {
   return (
-    <div className="ml-24 bg-gray-100 p-5  rounded-xl border-2">
+    <div className="ml-24 h-[400px] p-5 flex items-center  rounded-xl">
       <div className="flex items-center gap-x-10">
-        <p className="w-3/5 text-4xl font-semibold">
+        <p className="w-1/2 text-4xl font-semibold">
           Also, Get a Signed Certificate on successful Completion of the Course.
         </p>
-        <p className="w-2/5">
+        <p className="w-1/2">
           <Image
             src="/images/sertificate.jpeg"
             alt="Responsibility"
