f07aaa8985fb9ed482798ccae640f513337a345c
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-04-20 14:11:38 +0530

    card padding

--- app/(home)/dsa-live-classes/_components/FloatingHero.tsx
+++ app/(home)/dsa-live-classes/_components/FloatingHero.tsx
@@ -20,7 +20,7 @@ const FloatingHero = () => {
       <AnimatePresence>
         {isActiveFloatDlc && (
           <motion.div
-            className="bg-border-dlc lg:block bg-white shadow-xl border border-opacity-20 border-black hidden sticky top-24 p-4 bg-black-dlc z-10 -right-24  w-[360px]   h-[360px] text-black rounded-xl space-y-6"
+            className="bg-border-dlc lg:block bg-white shadow-xl border border-opacity-20 border-black hidden sticky top-32 p-4 bg-black-dlc z-10 -right-24  w-[360px]   h-[360px] text-black rounded-xl space-y-6"
             variants={floatingHeroVars}
             {...floatingHeroMotionProps}
           >
