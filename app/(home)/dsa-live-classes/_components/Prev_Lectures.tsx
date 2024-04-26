50e3ef7101a0caaae499886d499b4a67c29b764a
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-04-26 20:37:03 +0530

    set the margins

--- app/(home)/dsa-live-classes/_components/Prev_Lectures.tsx
+++ app/(home)/dsa-live-classes/_components/Prev_Lectures.tsx
@@ -1,10 +1,10 @@
 "use client";
 const PrevLectures = () => {
     return (
-      <div className="max-w-screen-xl p-8 mb-32 mt-10">
-        <h2 className="text-5xl font-semibold mb-20 pr-5 flex justify-end">Have a look at our previous live lectures</h2>
+      <div className="max-w-screen-xl p-8 mb-32 ml-20 mt-10  flex flex-col justify-center">
+        <h2 className="text-5xl font-semibold mb-20 pr-5 flex justify-center">Have a look at our previous live lectures</h2>
         <div className="prev-lectures w-full">
-          <div className="lecture-wrapper w-full flex justify-end gap-x-4">
+          <div className="lecture-wrapper w-full flex justify-center gap-x-4">
             <div className="lecture overflow-hidden rounded-lg shadow-lg w-2/5">
               <div className="aspect-video">
                 <iframe
