391b50583bc7730a1df6d39234bd832a05209753
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-04-28 16:45:59 +0530

    chnaged the price and optimized components loading

--- app/(home)/dsa-live-classes/_components/FloatingHero.tsx
+++ app/(home)/dsa-live-classes/_components/FloatingHero.tsx
@@ -20,15 +20,15 @@ const FloatingHero = () => {
       <AnimatePresence>
         {isActiveFloatDlc && (
           <motion.div
-            className="bg-border-dlc lg:block dark:bg-gray-800 bg-white shadow-xl border border-opacity-20 border-black hidden sticky top-32 p-4 bg-black-dlc z-10 -right-24  w-[360px]  h-[360px] text-black rounded-xl space-y-6"
+            className="bg-border-dlc lg:block dark:bg-gray-800 bg-white shadow-xl border border-opacity-20 border-black hidden sticky top-32 p-4 bg-black-dlc z-10 -right-24  w-[360px]  h-[340px] text-black rounded-xl space-y-6"
             variants={floatingHeroVars}
             {...floatingHeroMotionProps}
           >
             <div className="fl-ic gap-4">
               <span className="text-3xl font-semibold text-primaryPurple">
-                ₹2700
+                ₹999
               </span>
-              <del className="text-shark text-2xl">₹4800</del>
+              <del className="text-shark text-2xl">₹2499</del>
             </div>
             <p className="text-xl font-semibold dark:text-white">This Course Includes :</p>
             {courseDetailsHeroFloat.map((course) => (
@@ -36,11 +36,13 @@ const FloatingHero = () => {
                 {course}
               </p>
             ))}
+            <div>
             <Link href="https://pages.razorpay.com/dsa-soarx">
-              <button className="mt-4 signInbut hover:bg-purple-800 ease-in-out transition-all duration-300 w-full text-white  py-4 px-2 font-medium rounded-xl">
+              <button className=" signInbut hover:bg-purple-800 ease-in-out transition-all duration-300 w-full text-white px-2 font-medium rounded-xl">
                 Enroll Now
               </button>
             </Link>
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
