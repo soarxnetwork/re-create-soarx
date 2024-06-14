eb356b595eff1619b91687daf0496d8c15e7655b
Akshat <akshatjain481@gmail.com>
2024-06-14 22:11:49 +0530

    added the profile pics on the event registration pages

--- app/(home)/[slug]/page.tsx
+++ app/(home)/[slug]/page.tsx
@@ -1,12 +1,36 @@
 import EventPage from './EventPage';
 import { getEventBySlug } from '@/services/events';
-
+import { getUsersRegisteredForEvent } from '@/actions/registration';
 async function fetchData(slug: string) {
   const eventData = await getEventBySlug(slug);
   return eventData;
 }
 
+
+interface User {
+  id: string;
+  username: string | null;
+  password: string | null;
+  phone: string | null;
+  email: string | null;
+  image: string | null;
+  name: string | null;
+}
+
+
 export default async function Page({ params }: { params: { slug: string } }) {
   const eventData = await fetchData(params.slug);
-  return <EventPage event={eventData} />;
+  if(eventData){
+    const response = await getUsersRegisteredForEvent(eventData.id);
+
+if (response.error) {
+  // Handle error, for example:
+  console.error('Error fetching users for event:', response.message);
+  return(<div>Error fetching users for event</div>);
+}
+
+const users: User[] = response.data || [];
+
+  return <EventPage event={eventData} users={users} />;
+  }
 }
