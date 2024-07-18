bd98446530a6ff3158af79b4092cb3d6abb9a10a
AkshatJain481 <akshatjain481@gmail.com>
2024-07-18 21:43:29 +0530

    fixing bugs

--- components/Header.tsx
+++ components/Header.tsx
@@ -80,7 +80,7 @@ const Header = ({ admin }: User) => {
                 </Link>
               </li> */}
 
-              <HeaderDropdown />
+              {/* <HeaderDropdown /> */}
 
               {admin && admin !== Admin.User && (
                 <li
