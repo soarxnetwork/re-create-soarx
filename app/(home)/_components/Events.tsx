6cda94383f1ffc97983cb6cc20ba9ea04244c226
AkshatJain481 <akshatjain481@gmail.com>
2024-08-07 00:05:17 +0530

    fixed the event description display bugs

--- app/(home)/_components/Events.tsx
+++ app/(home)/_components/Events.tsx
@@ -19,9 +19,7 @@ const Events = ({ events }: EventProps) => {
   return (
     <section className="section  my-[10%] lg:pl-28 lg:pr-20">
       <div className="container mx-auto">
-        
         <h2 className="text-center text-[30px] sm:text-[40px] md:text-[60px] lg:text-[80px] pt-2 font-bold leading-normal">
-          
           {"Events".split("").map((child, idx) => (
             <span className={"hoverText"} key={idx}>
               {child}
@@ -29,11 +27,8 @@ const Events = ({ events }: EventProps) => {
           ))}
         </h2>{" "}
         <section className="all-events lg:grid grid-cols-3 gap-5 pt-16">
-          {events?.slice(0,3).map((e, index) => (
-            
-            <Link key={index} href={`${e.slug}`} >
-             
-
+          {events?.slice(0, 3).map((e, index) => (
+            <Link key={index} href={`${e.slug}`}>
               <div className="events-box aim-box dark:shadow-sm dark:shadow-purple-500 h-full ">
                 <div className="relative mt-10 hover-event-card-img h-fit overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border  w-full text-white">
                   <Image
@@ -47,13 +42,12 @@ const Events = ({ events }: EventProps) => {
                 <div className="pt-6 pl-6 pr-6">
                   <p className="text-slate-400 text-[12px]">Online | Free</p>
                   <h5 className="mb-2 pt-2 block  text-base font-medium leading-snug tracking-normal text-blue-gray-900 antialiased">
-                    
                     {e.title}
                   </h5>
                   <p className="block  text-sm font-light leading-relaxed text-inherit antialiased ">
-                   
-                    {e.description.slice(0,100) + '....'}
-                
+                    {e.home_description && (
+                      <>{e.home_description.slice(0, 100) + "...."}</>
+                    )}
                   </p>
                 </div>
                 <div className="flex   items-center pt-2 justify-between pr-2 pl-6 pb-2">
@@ -64,11 +58,9 @@ const Events = ({ events }: EventProps) => {
                   <button className="events-button">
                     <div className="button-box text-2xl text-[#c5c7ca]">
                       <span className="button-elem">
-                      
                         <MdArrowOutward />
                       </span>
                       <span className="button-elem">
-                      
                         <MdArrowOutward />
                       </span>
                     </div>
@@ -79,8 +71,11 @@ const Events = ({ events }: EventProps) => {
           ))}
         </section>
         <div className="flex items-center lg:mt-8 justify-center">
-         <Link href="/events">
-         <button className="signInbut mt-10"><p className="text-xl">View all</p></button></Link>
+          <Link href="/events">
+            <button className="signInbut mt-10">
+              <p className="text-xl">View all</p>
+            </button>
+          </Link>
         </div>
       </div>
     </section>
