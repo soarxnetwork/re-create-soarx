6c6241fdc69c0efb5f85e72e34aafa793dde250d
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-08-23 20:03:23 +0530

    download Registration data of users

--- app/(admin)/admin/events/_components/AdminEventsCard.tsx
+++ app/(admin)/admin/events/_components/AdminEventsCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { formatDate } from "@/utils/formatDate";
 import { truncateWord } from "@/utils/truncateWord";
 import { Event } from "@prisma/client";
@@ -18,6 +19,32 @@ const AdminEventsCard = ({
   date,
   confirm2,
 }: AdminEventsCardProps) => {
+
+  const handleDownload = async (id: string) => {
+    const response = await fetch("/api/DownloadRegisteredUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ eventId: id })
+    });
+
+    if (response.ok) {
+      const blob = await response.blob(); // Convert response to a blob
+      const url = window.URL.createObjectURL(blob); // Create a temporary URL for the blob
+      const a = document.createElement("a"); // Create an anchor element
+      a.href = url;
+      a.download = `registrations${Math.random() * 1000}dev.csv`; // Set the file name
+      document.body.appendChild(a); // Append the anchor to the document
+      a.click(); // Programmatically click the anchor to trigger the download
+      document.body.removeChild(a); // Remove the anchor from the document
+      window.URL.revokeObjectURL(url); // Release the object URL
+    } else {
+      console.error("Failed to fetch data:", response.status, response.statusText);
+    }
+  };
+
+
   return (
     <div className="events-box aim-box break-words w-full" key={id}>
       <div className="events-img">
@@ -37,6 +64,9 @@ const AdminEventsCard = ({
           {formatDate(date)}
         </p>
         <div className="flex items-center gap-3">
+        <button className="btn-primary bg-purple-500 mt-4" onClick={()=>handleDownload(id)}>
+            Download
+          </button>
           <button className="btn-primary red mt-4" onClick={() => confirm2(id)}>
             Delete
           </button>
