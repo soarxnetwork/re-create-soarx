7152b1a66b7d45544f338e93be207ddc40791b2e
Harsh <harshsik7890@gmail.com>
2024-09-20 10:21:06 +0530

    Revert "created functionality to send otp while signup"
    This reverts commit b3524c84a98e34e4cc9b211201502f8d9815b112.

--- app/(auth)/sign-up/_components/SignupForm.tsx
+++ app/(auth)/sign-up/_components/SignupForm.tsx
@@ -126,6 +126,7 @@ const SignupForm = ({ users }: SignupFormProps) => {
   };
 
   return (
+    // ---
     <form
       className="w-full max-w-lg mx-auto mt-8 p-6  rounded-lg shadow-md"
       onSubmit={handleSubmit(onSubmit)}
--- utils/generateOtp.ts
+++ utils/generateOtp.ts
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { addMinutes } from "date-fns";
 
-// Function to generate an OTP and expiration time
 export const generateOtp = () => {
   const otp = uuidv4().slice(0, 6); // Generate a 6-digit OTP
   const expiresAt = addMinutes(new Date(), 10); // OTP valid for 10 minutes
